e460167d66bff59978da0e01eaca088c765ca993
Manuel Vonthron <manuel.vonthron@acadis.org>
2012-07-26 00:21:02 -0400

    windows exe build

--- src/setup.py
+++ src/setup.py
@@ -3,18 +3,25 @@ import py2exe
 
 setup(
     name = "miCoach backup",
-    version = "1.0",
+    version = "0.9",
     description = "miCoach backup is a tool to save your workouts from Adidas miCoach to your computer",
     author = "Manuel Vonthron",
     author_email = "manuel.vonthron@acadis.org",
     url = "https://github.com/gliss/micoach-backup",
     
     # py2exe
-    windows = ["micoach-backup"],
+    windows = [{
+        "script": "micoach-backup",
+        "icon_resources": [(0, "gui/images/icon.ico")],
+        "copyright": "Copyright (c) 2012, Manuel Vonthron",
+        "version": "0.9",
+    }],
+    zipfile = None,
     options = {
         "py2exe": {
             "includes": ["PySide.QtXml", "configobj"],
-            "dll_excludes": ["MSVCP90.dll"]
+            "dll_excludes": ["MSVCP90.dll", "w9xpopen.exe"],
+            "bundle_files": 1
         }
     },
 )
