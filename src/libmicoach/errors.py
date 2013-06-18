5108ee59c17929fc0c23da5cf8549f5af2fbd2da
Manuel Vonthron <manuel.vonthron@acadis.org>
2013-06-17 23:01:21 -0400

    With new problems comes new error status

--- src/libmicoach/errors.py
+++ src/libmicoach/errors.py
@@ -2,5 +2,11 @@
 class LoginFailed(Exception):
     pass
 
+class NotAuthenticated(Exception):
+    pass
+
+class SessionTimeout(Exception):
+    pass
+
 class MissingCredential(Exception):
     pass
